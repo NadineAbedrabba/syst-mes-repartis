a67bf2c7e526544bdbf6dde9f565f3ea42458806
NadineAbedrabba <150744455+NadineAbedrabba@users.noreply.github.com>
2024-02-26 16:16:04 +0100

    Update RMIClient.java

--- RMIClient.java
+++ RMIClient.java
@@ -1,14 +1,1 @@
-import java.rmi.Naming;
 
-public class RMIClient {
-    public static void main(String[] args) {
-        try {
-            RemoteInterface remoteObject = (RemoteInterface) Naming.lookup("//localhost/RemoteObject");
-            String result = remoteObject.convertToUpper("hello I'm Nadine");
-            System.out.println("Result from server: " + result);
-        } catch (Exception e) {
-            System.err.println("Client exception: " + e.toString());
-            e.printStackTrace();
-        }
-    } 
-}
